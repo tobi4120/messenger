e146e267301724d58378688b3cdf18f899dd6172
Michael Tobis <tobi4120@mylaurier.ca>
2022-01-11 13:29:52 -0500

    added whitenoise to settings

--- msg_app/settings.py
+++ msg_app/settings.py
@@ -55,7 +55,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'msg_app.urls'
 
