c832f1b9f033dea38e6a37bcde94ee0a2b0f635c
Michael Tobis <tobi4120@mylaurier.ca>
2022-01-15 17:18:12 -0500

    message styling

--- frontend/src/components/home/messages/message.tsx
+++ frontend/src/components/home/messages/message.tsx
@@ -8,30 +8,37 @@ interface props {
 }
 
 const Message: React.FC<props> = (props) => {
-    const [className, setClassName] = useState<string>("messages__message");
-    let currentUser = false;
+    const [className, setClassName] = useState<string>("message");
+    const [currentUser, setCurrentUser] = useState<boolean>(false);
 
     // Let CSS know which message is sent by the current user so it can align it to the right
     useEffect(() => {
         if (props.message.user.email === props.user.email) {
-            setClassName(className + " currentUser")
-            currentUser = true;
+            setClassName(className + "CurrentUser")
+            setCurrentUser(true);
         }
     }, [])
 
+    if (currentUser)
+
     return (
         <div className={className}>
-            <div className="messages__message__left">
-                { currentUser ? 
-                    <p>{props.message.message}</p> : 
-                    <ProfilePic imageLocation={props.message.user.profile_pic} /> 
-                }
+            <div className={`${className}__right`}>
+                <ProfilePic imageLocation={props.message.user.profile_pic} /> 
             </div>
-            <div className="messages__message__right">
-            { currentUser ? 
-                    <ProfilePic imageLocation={props.message.user.profile_pic} />  : 
-                    <p>{props.message.message}</p>
-                }
+            <div className={`${className}__left`}>
+                <p className={`${className}__left__text`}>{props.message.message}</p> 
+            </div>
+        </div>
+    )
+
+    return (
+        <div className={className}>
+            <div className={`${className}__left`}>
+                <ProfilePic imageLocation={props.message.user.profile_pic} /> 
+            </div>
+            <div className={`${className}__right`}>
+                <p className={`${className}__right__text`}>{props.message.message}</p> 
             </div>
         </div>
     )
