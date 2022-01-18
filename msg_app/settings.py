130ea911e41fc142aba9afb0aca75e0b1878859b
Michael Tobis <tobi4120@mylaurier.ca>
2022-01-17 22:54:50 -0500

    newConvo mobile styling

--- msg_app/settings.py
+++ msg_app/settings.py
@@ -168,18 +168,18 @@ REST_KNOX = {
 ASGI_APPLICATION = 'msg_app.routing.application'
 
 # Development
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels.layers.InMemoryChannelLayer"
-#     }
-# }
-
-#Production
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
+
+#Production
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+#         },
+#     },
+# }
